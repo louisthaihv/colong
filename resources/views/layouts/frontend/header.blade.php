9e3103d05361cd8bdbd07a3905f3dc8c883c7c14
Nguyen Hao <Nguyen Hao>
2015-12-14 18:14:48 +0700

    code css for header: logo, menu

--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -1,19 +1,33 @@
     <header>
         <div class="container">
-            <h1><img src="{{ asset('frontend/images/logo.png') }}" alt="Cổ long online"/></h1>
-            <nav>
-                <ul>
-                    <li><a href="{{ route('frontend.index') }}">
-                        <img src="{{ asset('frontend/images/navbar/trang-chu.png') }}" alt="Trang chu"/>
-                    </a></li>
-                    @foreach($head_cats as $category)
-                    <li><a href="{{ route('frontend.category.show', $category->id) }}">
-                        <img src="{{ asset($category->image_url) }}" alt="{{ $category->name }}"/>
-                    </a></li>
-                    @endforeach
-                </ul>
-            </nav>
-
+            <div class="row">
+            <a class="logo pull-left" href="#"><img src="{{ asset('frontend/images/logo.png') }}" alt="Cổ long online"/></a>
+                <div class="col-sm-8 col-sm-offset-3 text-center">
+                    <nav id="header-menu">
+                        <ul>
+                            <li>
+                                <a href="{{ route('frontend.index') }}" class="background-menu home"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="background-menu feature"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="background-menu guide"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="background-menu activity"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="background-menu public"></a>
+                            </li>
+                            <!-- @foreach($head_cats as $category)
+                            <li><a href="{{ route('frontend.category.show', $category->id) }}">
+                                <img src="{{ asset($category->image_url) }}" alt="{{ $category->name }}"/>
+                            </a></li>
+                            @endforeach -->
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
-
     </header>
